66ede42335b2035c796caa5154bdbd07a8dec65f
Charlie Proctor <charlie@charlieproctor.com>
2016-01-10 17:34:20 -0500

    parse command-line arguments for creating and testing various graphs

--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,55 @@
 #include "cycle.h"
 #include "path.h"
 
+#include "string.h"
+#include "unistd.h"
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	// create and test a GRAPH
-	Graph *g = new Graph("first", DIRECTED);
-	Node *a = g->createVertex();
-	Node *b = g->createVertex();
+	// default arguments
+	int num = 5; 
+	string graph = "complete"; 
+	graph_t type = UNDIRECTED;
+	
+	// parse the command-line arguments
+	int c;
+	while ((c = getopt(argc, argv, "g:n:t:")) != -1) {
+		switch (c) {
+			case 'g':	// graph name
+				graph = string(optarg);
+				break;
+			case 'n':	// size of the graph
+				num = atoi(optarg);
+				break;
+			case 't':	// graph type (directed, undirected)
+				type = (!strcmp(optarg,"directed") ? DIRECTED : UNDIRECTED);
+				break;
+			case '?':
+				exit(EXIT_FAILURE);
+				break;
+		} 	
+	}
 
-	g->createEdge(a,b,DIRECTED);
+	// create and draw a COMPLETE GRAPH
+	if (graph == "complete") {
+		CompleteGraph *cg = new CompleteGraph(num, graph, type);
+		cout << "CompleteGraph: numVertices " << cg->numVertices() << ", numEdges " << cg->numEdges() << endl;
+		cg->dot();
 
-	cout << "Graph: numVertices " << g->numVertices() << ", numEdges " << g->numEdges() << endl;
-	g->dot();
+	// create and draw a CYCLE GRAPH
+	} else if (graph == "cycle") {
+		CycleGraph *cyg = new CycleGraph(num, graph, type);
+		cout << "CycleGraph: numVertices " << cyg->numVertices() << ", numEdges " << cyg->numEdges() << endl;
+		cyg->dot();
 
-	// create and test a COMPLETEGRAPH
-	CompleteGraph *cg = new CompleteGraph(5);
-	cout << "CompleteGraph: numVertices " << cg->numVertices() << ", numEdges " << cg->numEdges() << endl;
-	cg->dot();
-
-	// create and test a CYCLEGRAPH
-	CycleGraph *cyg = new CycleGraph(5);
-	cout << "CycleGraph: numVertices " << cyg->numVertices() << ", numEdges " << cyg->numEdges() << endl;
-	cyg->dot();
-
-	// create and test a PATHGRAPH
-	PathGraph *pg = new PathGraph(5);
-	cout << "PathGraph: numVertices " << pg->numVertices() << ", numEdges " << pg->numEdges() << endl;
-	pg->dot();
+	// create and draw a PATH GRAPH
+	} else if (graph == "path") {
+		PathGraph *pg = new PathGraph(num, graph, type);
+		cout << "PathGraph: numVertices " << pg->numVertices() << ", numEdges " << pg->numEdges() << endl;
+		pg->dot();
+	}
 
 	return 0;
 }
